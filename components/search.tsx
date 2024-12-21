dde01a77d2706c753474541f930b0d896039c8dd
Ni <daisytsai11336@gmail.com>
2024-12-21 16:46:46 +0800

    feat: Error message done. optimized: submit keywords after pressing down Enter.

--- components/search.tsx
+++ components/search.tsx
@@ -1,5 +1,6 @@
 import { useState, JSX } from "react";
 import { WeatherProps, ForecastProps } from "@/types/weather";
+import { toast, Toaster } from "react-hot-toast";
 
 type SearchProps = {
   handleTodayWeather: (todayWeather: WeatherProps | null) => void;
@@ -15,21 +16,28 @@ export default function Search({
   loading,
 }: SearchProps): JSX.Element {
   const [search, setSearch] = useState("");
+  const [isEmpty, setIsEmpty] = useState(false);
 
   const handleSearch = async (search: string) => {
+    if (search === "") {
+      setIsEmpty(true);
+      return;
+    };
     handleLoading(true);
-    try {
+    try { 
       const response = await fetch(`/api/weather?search=${search}`);
 
       if (!response.ok) {
-        throw new Error(`Failed to fetch weather data: ${response.status}`);
+        toast.error("查詢失敗，請輸入有效關鍵字，或更換關鍵字語言");
       }
 
       const data = await response.json();
+      setIsEmpty(false);
       handleTodayWeather(data.todayWeather);
       handleForecast(data.forecast);
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
+    } catch {
+      toast.error("查詢失敗");
+      setIsEmpty(false);
     } finally {
       handleLoading(false);
     }
@@ -37,15 +45,17 @@ export default function Search({
 
   return (
     <>
+      <Toaster />
       <nav className="px-3 py-2 bg-[#1eb0ff] rounded-md lg:mb-0 mb-2">
         <div className="flex items-center">
           <div className="pb-[0.125rem]">
             <input
               type="text"
-              className="rounded-sm text-sm text-[#016697] p-1"
+              className={`rounded-sm text-sm text-[#016697] p-1` + (isEmpty ? " border-2 border-[#ea6262]" : "")}
               placeholder="請輸入位置名稱"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && handleSearch(search)}
               disabled={loading}
             />
           </div>
@@ -57,6 +67,7 @@ export default function Search({
             查詢
           </button>
         </div>
+        {isEmpty && <p className="text-[#ea6262] p-1">請輸入位置名稱</p>}
       </nav>
     </>
   );
