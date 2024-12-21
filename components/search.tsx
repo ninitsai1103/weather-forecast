12fb2d3bc75a922fd2eb293110d2ce3dff49e11f
Ni <daisytsai11336@gmail.com>
2024-12-22 02:19:14 +0800

    feat: Searching keywords can be retrieved and deleted. feat: Clicking on a keyword can change the search target.

--- components/search.tsx
+++ components/search.tsx
@@ -1,9 +1,16 @@
-import { useState, JSX } from "react";
+import { useState, JSX, useEffect } from "react";
 import { WeatherProps, ForecastProps } from "@/types/weather";
 import { toast, Toaster } from "react-hot-toast";
 import { db } from "../firebase";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { useAuth } from "../context/authContext";
+import { IoIosCloseCircle } from "react-icons/io";
 
 type SearchProps = {
   handleTodayWeather: (todayWeather: WeatherProps | null) => void;
@@ -18,8 +25,9 @@ export default function Search({
   handleLoading,
   loading,
 }: SearchProps): JSX.Element {
-  const [search, setSearch] = useState("");
-  const [isEmpty, setIsEmpty] = useState(false);
+  const [search, setSearch] = useState<string>("");
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
+  const [searchHistory, setSearchHistory] = useState<{ id: string; search: string }[]>([]);
 
   const { isLogin, userEmail } = useAuth();
 
@@ -27,9 +35,9 @@ export default function Search({
     if (search === "") {
       setIsEmpty(true);
       return;
-    };
+    }
     handleLoading(true);
-    try { 
+    try {
       const response = await fetch(`/api/weather?search=${search}`);
 
       if (!response.ok) {
@@ -46,6 +54,18 @@ export default function Search({
           search: search,
         });
       }
+
+      setSearchHistory((prevHistory) => {
+        const existingSearch = prevHistory.find(
+          (item) => item.search === search
+        );
+        if (existingSearch) {
+          return prevHistory;
+        } else {
+          return [...prevHistory, { id: crypto.randomUUID(), search: search }];
+        }
+      });
+
     } catch {
       toast.error("查詢失敗");
       setIsEmpty(false);
@@ -53,6 +73,39 @@ export default function Search({
       handleLoading(false);
     }
   };
+
+  const deleteSearchHistory = async (id: string) => {
+    try {
+      if (!isLogin || !userEmail) return;
+      await deleteDoc(doc(db, "users", userEmail, "search", id));
+      setSearchHistory((prevHistory) =>
+        prevHistory.filter((item) => item.id !== id)
+      );
+    } catch {
+      toast.error("刪除搜尋紀錄失敗");
+    }
+  };
+
+  useEffect(() => {
+    if (isLogin && userEmail) {
+      const getSearchHistory = async () => {
+        try {
+          const getSearchData = await getDocs(
+            collection(db, "users", userEmail, "search")
+          );
+          const history: { id: string; search: string }[] = [];
+
+        getSearchData.forEach((doc) => {
+          history.push({ id: doc.id, search: doc.data().search });
+        });
+          setSearchHistory(history);
+        } catch {
+          toast.error("取搜尋紀錄失敗");
+        }
+      };
+      getSearchHistory();
+    }
+  }, [isLogin, userEmail]);
 
   return (
     <>
@@ -62,7 +115,9 @@ export default function Search({
           <div className="pb-[0.125rem]">
             <input
               type="text"
-              className={`rounded-sm text-sm text-[#016697] p-1 ${isEmpty ? " border-2 border-[#ea6262]" : ""}`}
+              className={`rounded-sm text-sm text-[#016697] p-1 ${
+                isEmpty ? " border-2 border-[#ea6262]" : ""
+              }`}
               placeholder="請輸入位置名稱"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
@@ -78,7 +133,23 @@ export default function Search({
             查詢
           </button>
         </div>
-        {isEmpty && <p className="bg-[#ea6262] mt-1 px-2 rounded-sm inline-block">請輸入位置名稱</p>}
+        {isEmpty && (
+          <p className="bg-[#ea6262] mt-1 px-2 rounded-sm inline-block">
+            請輸入位置名稱
+          </p>
+        )}
+        <div className="flex flex-wrap lg:max-w-[12rem] max-w-full">
+          {searchHistory.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center text-md mt-1 bg-[#E1CEEF] text-[#3F1E68] px-1 mr-1 mb-1 rounded-sm cursor-pointer"
+              onClick={() => handleSearch(item.search)}
+            >
+              <p className="mr-2">{item.search}</p>
+              <IoIosCloseCircle onClick={() => deleteSearchHistory(item.id)} />
+            </div>
+          ))}
+        </div>
       </nav>
     </>
   );
